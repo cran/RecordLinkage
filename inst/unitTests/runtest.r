40e3a146ed1265897c9baf03303c11bd4823d9ec
Andreas Borg <borga@uni-mainz.de>
2015-01-15 00:00:00 +0000

    version 0.4-7

--- inst/unitTests/runtest.r
+++
@@ -1,7 +0,0 @@
-#!/usr/local/bin/Rscript
-library(RUnit)
-library(RecordLinkage)
-testSuite <- defineTestSuite("Test Suite for Package RecordLinkage",
-  ".")
-testResult <- runTestSuite(testSuite)
-printHTMLProtocol(testResult, fileName="protocol.html")
